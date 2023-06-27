87ba3f59e8f3d79b7792365fc9cf00533027ac51
arsham-gheibi <gheibi.arsham@gmail.com>
2023-06-27 19:38:34 +0330

    Update linting

--- main.py
+++ main.py
@@ -88,7 +88,7 @@ async def show_Receipt(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await query.answer()
     data = int(query.data)
 
-    item = MENU_RESTURANT.get(data) if item is not None\
+    item = MENU_RESTURANT.get(data) if item is not None \
         else MENU_CAFE.get(data)
 
     user_orders = ORDERS.get(query.from_user.id, [])
